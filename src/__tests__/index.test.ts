28fdacb725f1dbb1e073f2748b42c8478c53f40c
osneem <oskar.neemre@gmail.com>
2021-09-06 15:03:23 +0300

    Tests, fixed arrayRemoveWhitespace

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,12 @@
 import { type } from "os";
-import { arrayRemoveWhitespace, stripSymbols, commandSequencer } from "../index";
+import { arrayRemoveWhitespace, stripSymbols, commandSequencer, stripLineBreaks } from "../index";
+
+
 
 test('basic', () => {
-  expect(stripSymbols("Hi, my name is Oskar and this is a string, which has too many symbols!")).toBe("Hi my name is Oskar and this is a string which has too many symbols");
+  expect(stripSymbols('Hi, my name is Oskar and this is a string, which has too many symbols!')).toEqual('Hi my name is Oskar and this is a string which has too many symbols');
 });
 
 test('basic', () => {
-  expect(arrayRemoveWhitespace(["Hi", " ", "Hello"])).toBe(["Hi", "Hello"]);
+  expect(arrayRemoveWhitespace(['Hi', ' ', 'Hello'])).toEqual(['Hi', 'Hello']);
 });
