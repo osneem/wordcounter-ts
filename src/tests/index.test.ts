98e3e73c8ef12d84422ae144f846e01f5922e5de
osneem <55879952+osneem@users.noreply.github.com>
2021-09-06 13:25:03 +0300

    Update index.test.ts

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,5 +1,5 @@
 import { arrayRemoveWhitespace, stripSymbols } from "../index";
 
 test('basic', () => {
-    expect(stripSymbols("lol minu nimi. . . on idikas!")).toBe("lol minu nimi on idikas");
+    expect(stripSymbols("lol  .lol")).toBe("lol  lol");
   });
