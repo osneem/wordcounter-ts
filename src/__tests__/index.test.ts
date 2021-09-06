67f6b604ac0e5d7e7fe8f914cb84f0571af18577
osneem <oskar.neemre@gmail.com>
2021-09-06 13:34:20 +0300

    Jest test suite problem

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import { arrayRemoveWhitespace, stripSymbols } from "../index";
+
+test('basic', () => {
+    expect(stripSymbols("lol minu nimi. . . on idikas!")).toBe("lol minu nimi   on idikas");
+  });
