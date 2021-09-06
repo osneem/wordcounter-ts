55ac8264d012e240dca7955cf269ba2f923f306a
osneem <oskar.neemre@gmail.com>
2021-09-06 15:38:12 +0300

    config file into typescript

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,14 @@
 import { type } from "os";
 import { arrayRemoveWhitespace, stripSymbols, commandSequencer, stripLineBreaks } from "../index";
 
-test('basic', () => {
+test('testLineBreakRemoval', () => {
   expect(stripLineBreaks('Replace new line\n with whitespace!')).toEqual('Replace new line  with whitespace!');
 });
 
-test('basic', () => {
-  expect(stripSymbols('Hi, this is a string, which has too many symbols!')).toEqual('Hi  this is a string which has too many symbols');
+test('testSymbolRemoval', () => {
+  expect(stripSymbols('Hi, this is a string, which has too many symbols!')).toEqual('Hi this is a string which has too many symbols');
 });
 
-test('basic', () => {
+test('testWhitespaceRemoval', () => {
   expect(arrayRemoveWhitespace(['Hi', ' ', 'Hello'])).toEqual(['Hi', 'Hello']);
 });
